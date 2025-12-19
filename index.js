187d765ab97e9b24008ae626f62aa7574a1b3c67
Usama Sajjad <usama.sajjad401@gmail.com>
2025-12-19 21:31:28 +0500

    added files

--- index.js
+++
@@ -1,3 +0,0 @@
-const express = require('express')
-const app= express.app();
-app.listen(8080);
---
+++ project/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const PersonSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model('Person', PersonSchema);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB connected"))
+  .catch(() => console.log("DB connect failed"));
+
+app.post("/", (req, res) => {
+  const p1 = new Person({
+    name: req.body.name,
+    age: req.body.age
+  });
+
+  p1.save()
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+app.get("/:input", (req, res) => {
+  const input = req.params.input;
+
+  if (!isNaN(input)) {
+    Person.find({ age: Number(input) })
+      .then(result => res.json(result))
+      .catch(err => res.json(err));
+  } else {
+    Person.find({ name: { $regex: new RegExp(input, "i") } })
+      .then(result => res.json(result))
+      .catch(err => res.json(err));
+  }
+});
+
+module.exports = app;
